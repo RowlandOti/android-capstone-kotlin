5fd7b9570327891338d1227e80f587fbb3495804
Otieno Rowland <r.otieno@kokonetworks.com>
2020-05-31 10:52:55 +0300

    chore: add api endpoint calls

--- app/src/main/java/com/example/android/politicalpreparedness/network/CivicsApiService.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/CivicsApiService.kt
@@ -1,16 +1,27 @@
 package com.example.android.politicalpreparedness.network
 
+import com.example.android.politicalpreparedness.network.jsonadapter.ElectionAdapter
+import com.example.android.politicalpreparedness.network.models.ElectionResponse
+import com.example.android.politicalpreparedness.network.models.RepresentativeResponse
+import com.example.android.politicalpreparedness.network.models.VoterInfoResponse
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+import java.util.*
 
 private const val BASE_URL = "https://www.googleapis.com/civicinfo/v2/"
 
-// TODO: Add adapters for Java Date and custom adapter ElectionAdapter (included in project)
 private val moshi = Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
+        .add(Rfc3339DateJsonAdapter().nullSafe())
+        .add(ElectionAdapter())
         .build()
 
 private val retrofit = Retrofit.Builder()
@@ -25,11 +36,14 @@ private val retrofit = Retrofit.Builder()
  */
 
 interface CivicsApiService {
-    //TODO: Add elections API Call
+    @GET("elections")
+    fun getElection() : Call<List<ElectionResponse>>
 
-    //TODO: Add voterinfo API Call
+    @GET("voterinfo")
+    fun getVoterInfo(@Query("address") address: String, @Query("electionId") electionId: Int) : Call<VoterInfoResponse>
 
-    //TODO: Add representatives API Call
+    @GET("representatives")
+    fun getRepresentatives(@Query("address") address: String): Call<RepresentativeResponse>
 }
 
 object CivicsApi {
