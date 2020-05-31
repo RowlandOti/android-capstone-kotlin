232e135a1550f8732e922e441e98afb59426a2ed
Otieno Rowland <r.otieno@kokonetworks.com>
2020-05-31 10:37:53 +0300

    todo: added api key

--- app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
@@ -6,7 +6,7 @@ class CivicsHttpClient: OkHttpClient() {
 
     companion object {
 
-        const val API_KEY = "" //TODO: Place your API Key Here
+        const val API_KEY = "AIzaSyBccycgnfIq641MVr93Pa2VsQY8seit31Y"
 
         fun getClient(): OkHttpClient {
             return Builder()
