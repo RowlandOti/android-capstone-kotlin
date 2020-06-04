bee740d6923a4b4cef712f0f707122448e5305b6
Otieno Rowland <r.otieno@kokonetworks.com>
2020-06-04 22:17:50 +0300

    feat: add representative and upcoming elections UI elements

--- app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
@@ -5,18 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.android.politicalpreparedness.databinding.FragmentElectionBinding
 
 class ElectionsFragment: Fragment() {
-
-    //TODO: Declare ViewModel
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
         //TODO: Add ViewModel values and create ViewModel
+        val electionsViewModel : ElectionsViewModel by viewModels()
 
         //TODO: Add binding values
+        val binding = FragmentElectionBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        binding.viewmodel = electionsViewModel
 
         //TODO: Link elections to voter info
 
@@ -24,6 +28,7 @@ class ElectionsFragment: Fragment() {
 
         //TODO: Populate recycler adapters
 
+        return binding.root
     }
 
     //TODO: Refresh adapters when fragment loads
