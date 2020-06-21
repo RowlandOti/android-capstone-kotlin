93c35d92fc1f725b6aa6adb2680fb7fa38e23e20
Otieno Rowland <r.otieno@kokonetworks.com>
2020-06-21 23:08:08 +0300

    feat: handle back navigation correctly

--- app/src/main/java/com/example/android/politicalpreparedness/MainActivity.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.android.politicalpreparedness
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.android.politicalpreparedness.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setupActionBarWithNavController(this,navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
