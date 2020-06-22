a47a3cf576afd18ef79de5c07ae25d4bf3bbe76e
Otieno Rowland <r.otieno@kokonetworks.com>
2020-06-22 16:39:49 +0300

    chore: remove commented out code

--- app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeViewModel.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeViewModel.kt
@@ -54,23 +54,4 @@ class RepresentativeViewModel : ViewModel() {
             }
         }
     }
-
-    /**
-     *  The following code will prove helpful in constructing a representative from the API. This code combines the two nodes of the RepresentativeResponse into a single official :
-
-    val (offices, officials) = getRepresentativesDeferred.await()
-    _representatives.value = offices.flatMap { office -> office.getRepresentatives(officials) }
-
-    Note: getRepresentatives in the above code represents the method used to fetch data from the API
-    Note: _representatives in the above code represents the established mutable live data housing representatives
-
-     */
-
-    //TODO: Create function get address from geo location
-/*    fun getAddress(location: Location): Address {
-
-    }*/
-
-    //TODO: Create function to get address from individual fields
-
 }
